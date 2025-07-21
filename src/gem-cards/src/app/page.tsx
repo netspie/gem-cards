4e10406877dea35c374ac3940db3f7d018768997
Dariusz Luśnia <dariusz.lusnia@gmail.com>
2025-07-21 11:00:10 +0900

    Improved cards

--- src/gem-cards/src/app/page.tsx
+++ src/gem-cards/src/app/page.tsx
@@ -8,6 +8,7 @@ import {
   Diamond,
   DiamondOutlined,
   FireExtinguisher,
+  LocalFireDepartment,
   PlusOne,
   Remove,
 } from "@mui/icons-material";
@@ -15,13 +16,19 @@ import {
   Button,
   Chip,
   Divider,
+  FormControl,
+  InputLabel,
+  MenuItem,
   Paper,
+  Select,
+  SelectChangeEvent,
   Stack,
   TextField,
   Tooltip,
   Typography,
   useTheme,
 } from "@mui/material";
+import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 export default function Home() {
@@ -30,7 +37,7 @@ export default function Home() {
     "hover:shadow-2xl transition-all duration-200 ease-in-out";
 
   return (
-    <div className="relative flex flex-col xl:flex-row gap-12 xl:gap-22 w-full h-fit items-center xl:justify-center mt-12 md:mt-36 xl:px-40 mb-24 xl:mb-0">
+    <div className="relative flex flex-col gap-12 xl:gap-22 w-full h-fit items-center xl:justify-center mt-12 md:mt-36 xl:px-40 mb-24 xl:mb-0">
       <div
         className="fixed -translate-[60%] top-0 rounded-full -z-10 opacity-10"
         style={{
@@ -91,9 +98,19 @@ export default function Home() {
           </Stack>
         </div>
       </div>
-      <Stack className="relative w-fit gap-3 justify-center items-center overflow-clip px-20 xl:px-0">
+      <div className="flex flex-col gap-4 w-full md:w-1/2 px-8">
+        <div
+          className="p-4 rounded-xl w-full"
+          style={{ background: theme.palette.primary.main }}
+        >
+          <Typography className="text-white text-center font-bold">
+            Don&apos;t make learning a chore! Make it fun! Make it a game!
+          </Typography>
+        </div>
+      </div>
+      <Stack className="relative flex-row flex-wrap w-fit gap-3 justify-center items-center overflow-clip px-20 xl:px-0">
         {/* <Card front="火" frontSize={36} name="Fire" gemCount={4} xp={200} /> */}
-        {/* <div
+        <div
           className="p-3 rounded-2xl "
           style={{ background: theme.palette.primary.main }}
         >
@@ -105,37 +122,64 @@ export default function Home() {
             gemCount={4}
             xp={200}
           />
-        </div> */}
+        </div>
+        <div
+          className="p-3 rounded-2xl "
+          style={{ background: theme.palette.primary.main }}
+        >
+          <Card
+            front={
+              <LocalFireDepartment
+                style={{ width: "40cqw", height: "40cqw" }}
+                color="primary"
+              />
+            }
+            frontSize={12}
+            frontClassName="font-bold"
+            name="火"
+            gemCount={4}
+            xp={200}
+          />
+        </div>
         <div
           className="p-3 rounded-2xl "
           style={{ background: theme.palette.primary.main }}
         >
           <Stack className="xl:w-[350px] aspect-[4/5] bg-white rounded-xl p-3 gap-2">
-            <Typography className="w-ful text-center uppercase font-bold">Create your first flashcard</Typography>
-            <div className="flex gap-2">
-              <TextField
-                id="front"
-                label="Question"
-                size="small"
-                className="w-full"
-                defaultValue="What is the capital of Australia?"
-                focused
-              />
-            </div>
-            <div className="flex gap-2">
-              <TextField
-                id="back"
-                label="Answer"
-                size="small"
-                className="w-full"
-                defaultValue="Canberra"
-                focused
-              />
-            </div>
+            <Typography className="w-ful text-center uppercase font-bold">
+              Create Simple Gemcard
+            </Typography>
+            <SpyTextField
+              label="Question"
+              defaultValue="What is the capital of Australia?"
+            />
+            <SpyTextField label="Answer" defaultValue="Canberra" />
             <Button variant="contained">Create</Button>
           </Stack>
         </div>
-        <div className="flex gap-2 w-full">
+        <div
+          className="p-3 rounded-2xl "
+          style={{ background: theme.palette.primary.main }}
+        >
+          <Stack className="xl:w-[350px] aspect-[4/5] bg-white rounded-xl p-3 gap-2">
+            <Typography className="w-ful text-center uppercase font-bold">
+              Create Multiside Gemcard
+            </Typography>
+            <div className="flex gap-2">
+              <SpyTextField label="Original" defaultValue="水" />
+              <SpySelect label="Type">
+                <MenuItem value="Character">Character</MenuItem>
+                <MenuItem value="Text">Text</MenuItem>
+                <MenuItem value="Image">Image</MenuItem>
+                <MenuItem value="Audio">Audio</MenuItem>
+              </SpySelect>
+            </div>
+            <SpyTextField label="Reading" defaultValue="みず" />
+            <SpyTextField label="Meaning" defaultValue="Water" />
+            <Button variant="contained">Create</Button>
+          </Stack>
+        </div>
+        {/* <div className="flex gap-2 w-full">
           <Button variant="contained" className="w-fit">
             <ArrowLeft />
           </Button>
@@ -145,7 +189,7 @@ export default function Home() {
           <Button variant="contained" className="w-fit">
             <ArrowRight />
           </Button>
-        </div>
+        </div> */}
         {/* <Card
           front="水"
           name="Water"
@@ -205,8 +249,55 @@ export default function Home() {
   );
 }
 
+type SpySelectProps = {
+  label: string;
+  children?: React.ReactNode;
+};
+
+function SpySelect(props: SpySelectProps) {
+  const [value, setValue] = useState<string>();
+
+  return (
+    <FormControl className="min-w-1/2" focused>
+      <InputLabel id={props.label}>{props.label}</InputLabel>
+      <Select
+        labelId={props.label}
+        id={props.label}
+        label={props.label}
+        size="small"
+        className="text-black"
+        color="primary"
+        value={value}
+        onChange={(ev: SelectChangeEvent) =>
+          setValue(ev.target.value as string)
+        }
+      >
+        {props.children}
+      </Select>
+    </FormControl>
+  );
+}
+
+type SpyTextFieldProps = {
+  label: string;
+  defaultValue?: string;
+};
+
+function SpyTextField(props: SpyTextFieldProps) {
+  return (
+    <TextField
+      id={props.label}
+      label={props.label}
+      size="small"
+      className="w-full"
+      defaultValue={props.defaultValue}
+      focused
+    />
+  );
+}
+
 type CardProps = {
-  front: string;
+  front: string | React.ReactNode;
   frontSize?: number;
   frontClassName?: string;
 
@@ -261,12 +352,16 @@ function Card(props: CardProps) {
             layout="fill"
             objectFit="cover"
           /> */}
-        <Typography
-          style={{ fontSize: `${props.frontSize || 10}cqw` }}
-          className={twMerge(``, props.frontClassName)}
-        >
-          {props.front}
-        </Typography>
+        {typeof props.front === "string" ? (
+          <Typography
+            style={{ fontSize: `${props.frontSize || 10}cqw` }}
+            className={twMerge(``, props.frontClassName)}
+          >
+            {props.front}
+          </Typography>
+        ) : (
+          props.front
+        )}
       </div>
       {/* <Divider /> */}
       <div className="w-full shrink-1 grow-0 flex flex-col gap-2 items-center">
